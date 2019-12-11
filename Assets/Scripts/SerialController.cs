016266a9643e6a87fa8eae347199a5509be8e043
yamachu <yamachu.dev@gmail.com>
2019-12-12 06:36:45 +0900

    [WIP] Add base SerialManager
    Impl input subscriber

---
+++ Assets/Scripts/SerialController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class SerialController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Todo: Open Serial Port
+        var inputManager = GameObject.FindGameObjectWithTag("InputManager");
+        if (inputManager == null)
+        {
+            Debug.LogError("InputManager is not found");
+            return;
+        }
+        var inputSubscriber = inputManager.GetComponent<VRControllerInputSubscriber>();
+        // Todo: keep 60FPS?
+        inputSubscriber.OnInputChanged.Subscribe(param =>
+        {
+            Debug.Log(param);
+            // Todo: Send
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
