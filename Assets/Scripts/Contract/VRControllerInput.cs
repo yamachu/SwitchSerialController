fb19df300ae45f4588e0ec2d8a458e6469d7d07a
yamachu <yamachu.dev@gmail.com>
2019-12-12 06:19:42 +0900

    Add input watcher

---
+++ Assets/Scripts/Contract/VRControllerInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Contract
+{
+    public struct VRControllerInput
+    {
+        public bool ButtonA;
+        public bool ButtonB;
+        public bool ButtonX;
+        public bool ButtonY;
+        public bool TriggerR;
+        public bool TriggerL;
+        public bool GripR;
+        public bool GripL;
+        public bool ButtonR;
+        public bool ButtonL;
+        public float XR;
+        public float YR;
+        public float XL;
+        public float YL;
+        // Todo: Direction, Velocity...
+    }
+}
+
---
+++ Assets/Scripts/VRControllerInputSubscriber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Contract;
+using UniRx;
+using UnityEngine;
+
+public class VRControllerInputSubscriber : MonoBehaviour
+{
+    private Subject<VRControllerInput> inputSubject = new Subject<VRControllerInput>();
+
+    public IObservable<VRControllerInput> OnInputChanged
+    {
+        get { return inputSubject; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputSubject.OnNext(new VRControllerInput
+        {
+            ButtonA = OVRInput.Get(OVRInput.Button.One),
+            ButtonB = OVRInput.Get(OVRInput.Button.Two),
+            ButtonX = OVRInput.Get(OVRInput.Button.Three),
+            ButtonY = OVRInput.Get(OVRInput.Button.Four),
+            ButtonR = OVRInput.Get(OVRInput.Button.SecondaryThumbstick),
+            ButtonL = OVRInput.Get(OVRInput.Button.PrimaryThumbstick),
+            GripR = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger),
+            GripL = OVRInput.Get(OVRInput.Button.PrimaryHandTrigger),
+            TriggerR = OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger),
+            TriggerL = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger),
+            XR = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x,
+            YR = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y,
+            XL = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x,
+            YL = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y,
+        });
+    }
+}
