6e0e1ddda686a54ad2007c7582c2e18ffa35f40d
yamachu <yamachu.dev@gmail.com>
2019-12-12 05:24:27 +0900

    Add webcam renderer

---
+++ Assets/Scripts/WebcameraCapture.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcameraCapture : MonoBehaviour
+{
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+    public int Device = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var devices = WebCamTexture.devices;
+        if (devices.Length == 0)
+        {
+            Debug.LogWarning("No camera devices found");
+            return;
+        }
+
+        if (devices.Length < Device)
+        {
+            Debug.LogError($"Cannot attach device: {Device}");
+        }
+
+        var cameraDevice = devices[Device];
+        Debug.Log($"Use device: {cameraDevice.name}");
+
+        var webcamTexture = new WebCamTexture(cameraDevice.name);
+        GetComponent<Renderer>().material.mainTexture = webcamTexture;
+
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
