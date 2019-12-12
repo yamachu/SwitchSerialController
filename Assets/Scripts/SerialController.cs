76e664b63f4de3beed0efc4574acde499f5cdc53
yamachu <yamachu.dev@gmail.com>
2019-12-12 09:47:29 +0900

    maybe work...

--- Assets/Scripts/SerialController.cs
+++ Assets/Scripts/SerialController.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using System.IO.Ports;
 
 public class SerialController : MonoBehaviour
 {
+    public string PortName = "COM3";
+    public int BaudRate = 9600;
+
+    private SerialPort serialPort;
+    private bool isRunning = false;
+
+    private Subject<int> readByteSubject = new Subject<int>();
+
     // Start is called before the first frame update
     void Start()
     {
-        // Todo: Open Serial Port
+        Open();
+
         var inputManager = GameObject.FindGameObjectWithTag("InputManager");
         if (inputManager == null)
         {
@@ -20,8 +30,69 @@ public class SerialController : MonoBehaviour
         inputSubscriber.OnInputChanged.Subscribe(param =>
         {
             Debug.Log(param);
-            // Todo: Send
+            Write(new[] { 'a', 'b', 'c' });
         });
+
+        readByteSubject.Subscribe(b =>
+        {
+            Debug.LogFormat("Read: :X", b);
+        });
+    }
+
+    void OnDestroy()
+    {
+        Close();
+    }
+
+    private void Open()
+    {
+        serialPort = new SerialPort(PortName, BaudRate, Parity.None, 8, StopBits.One);
+        serialPort.ReadTimeout = 20;
+        serialPort.Open();
+
+        isRunning = true;
+
+        Observable.Start(() =>
+        {
+            while (isRunning && serialPort != null && serialPort.IsOpen)
+            {
+                try
+                {
+                    readByteSubject.OnNext(serialPort.ReadByte());
+
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogWarning(e.Message);
+                }
+            }
+        })
+        .ObserveOnMainThread()
+        .Subscribe((_) =>
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                serialPort.Dispose();
+            }
+        });
+    }
+
+    private void Close()
+    {
+        isRunning = false;
+    }
+
+    private void Write(char[] messages)
+    {
+        try
+        {
+            serialPort.Write(messages, 0, messages.Length);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
     }
 
     // Update is called once per frame
